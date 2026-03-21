0b89b52351abc874eac294e0cd73e37c6cfa0051
Eliezer Ribeiro <146236086+eliezershell@users.noreply.github.com>
2026-03-21 01:42:49 -0300

    Create install.sh

---
+++ environment-minikube/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+
+sudo apt update -y
+git clone https://github.com/eliezershell/docker.git
+chmod +x ./docker/instalador_docker.sh
+./docker/instalador_docker.sh
